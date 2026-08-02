80eefbd709ffc96e00a2c3fabfd4e3515923ff2e
cryptoocheck <cryptoocheck@gmail.com>
2026-08-02 23:38:43 +0300

    Update app.py

--- app.py
+++ app.py
@@ -42,8 +42,14 @@ if st.sidebar.button("Analizi Başlat"):
             # 2. Gemini AI Analizi
             st.subheader("🤖 AI Ajanının Kanal Strateji Raporu")
             with st.spinner("Yapay zeka verileri inceliyor..."):
-                genai.configure(api_key=gemini_key)
-                model = genai.GenerativeModel('gemini-2.5-flash')
+               genai.configure(api_key=gemini_key)
+        
+        # Kullanılabilir modelleri otomatik bul
+        available_models = [m.name for m in genai.list_models() if 'generateContent' in m.supported_generation_methods]
+        
+        # En güncel çalışan modeli seç
+        selected_model = available_models[0]
+        model = genai.GenerativeModel(selected_model)
                 
                 prompt = f"""
                 Sen profesyonel bir YouTube Kripto Kanalı Stratejistisin.
